61c8c1874af888e89f4b8fedefbb17111ba685b0
Jordan Lee <jtlee@rmgj2.com>
2022-10-02 21:59:48 -0500

    work

---
+++ scripts/vendor.js
@@ -0,0 +1,15 @@
+// Destinations Object
+const destinations = [
+    { name: 'Paris', pic: 'pics/paris', bestSeason: 'Fall', price: '$$$', activities: 'Sightseeing, Eating at great restaurants, See museums' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+    { name: '', pic: null, bestSeason: '', price: '', activities: '' },
+]
+
+// Const Declarations
